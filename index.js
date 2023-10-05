8956b0184e5865cf68ffa1f9a668d7061b6e3a5c
0xsnowman <eth.snowman@gmail.com>
2023-10-06 07:54:15 +0800

    fix

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const app = express();
 
 // Use the built-in middleware for parsing JSON and URL-encoded request bodies
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 // Define a route for handling POST requests
 app.post('/tickets', (req, res) => {
   // Extract the 'email' and 'ticket_id' parameters from the request body
-  const email = req.query.email;
-  const ticketId = req.query.ticket_id;
+  console.log(req.body);
+  const email = req.body.email;
+  const ticket_id = req.body.ticket_id;
 
   // Do something with the parameters (e.g. save to database, send email, etc.)
-  console.log(`Received ticket data: email=${email}, ticket_id=${ticketId}`);
+  console.log(`Received ticket data: email=${email}, ticket_id=${ticket_id}`);
 
   // Send a response to the client
-  res.status(200).send(`Ticket data received. Payload: {${email}, ${ticketId}}`);
+  res.status(200).send(`Ticket data received. Payload: {${email}, ${ticket_id}}`);
 });
 
 // Start the server
