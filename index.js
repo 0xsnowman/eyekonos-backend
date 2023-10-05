0c6855940ee0f6da52a1f6b3a625dd252b761dc4
0xsnowman <eth.snowman@gmail.com>
2023-10-06 01:18:28 +0800

    post

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/', (req, res) => {
+app.post('/api/', (req, res) => {
     res.send('api integration')
 })
 
