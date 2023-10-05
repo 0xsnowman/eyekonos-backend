afeceb54fc0aaa80755bf7331ee2568bcb6901cb
0xsnowman <eth.snowman@gmail.com>
2023-10-06 01:37:37 +0800

    post

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.post('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.post('/api/', (req, res) => {
-    res.send('api integration')
-})
+// Use the built-in middleware for parsing JSON and URL-encoded request bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Define a route for handling POST requests
+app.post('/tickets', (req, res) => {
+  // Extract the 'email' and 'ticket_id' parameters from the request body
+  const email = req.query.email;
+  const ticketId = req.query.ticket_id;
+
+  // Do something with the parameters (e.g. save to database, send email, etc.)
+  console.log(`Received ticket data: email=${email}, ticket_id=${ticketId}`);
+
+  // Send a response to the client
+  res.status(200).send(`Ticket data received. Payload: {${email}, ${ticketId}}`);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
