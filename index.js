894c9dcea8d1585da8c3cb5983579831a9323098
0xsnowman <eth.snowman@gmail.com>
2023-10-07 04:54:33 +0800

    code-param-add

--- index.js
+++ index.js
@@ -30,11 +30,12 @@ app.get('/authorize', (req, res) => {
   const state = req.query.state;
   const redirectUri = req.query.redirect_uri;
   const responseType = req.query.response_type;
+  const code = req.query.code;
 
   // Check with process.env.CLIENT_ID and process.env.CLIENT_SECRET
-  console.log(`Authorized successfully, payload: {${clientId}, ${state}, ${redirectUri}, ${responseType}}`);
+  console.log(`Authorized successfully, payload: {${clientId}, ${state}, ${redirectUri}, ${responseType}, ${code}}`);
 
-  const authorizeUrl = `${process.env.ZAPIER_REDIRECT_URI}?client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}`;
+  const authorizeUrl = `${process.env.ZAPIER_REDIRECT_URI}?client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&code=${code}`;
 
   // Send an access token
   res.redirect(authorizeUrl);
