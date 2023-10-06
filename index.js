4a916bf25141740d666046140b6b3b9ae4af466b
0xsnowman <eth.snowman@gmail.com>
2023-10-06 12:50:02 +0800

    access_token

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+
 const app = express();
 
 // Use the built-in middleware for parsing JSON and URL-encoded request bodies
@@ -21,7 +23,47 @@ app.post('/tickets', (req, res) => {
   res.status(200).send(`Ticket data received. Payload: {${email}, ${ticket_id}}`);
 });
 
+// OAuth2 authentication
+app.post('/auth', (req, res) => {
+  console.log(req.body);
+  const client_id = req.body.client_id;
+  const client_secret = req.body.client_secret;
+  const res_token = 'fail';
+
+  // Check with process.env.CLIENT_ID and process.env.CLIENT_SECRET
+  console.log(`Authorized successfully, payload: {${client_id}, ${client_secret}}`);
+
+  // Send an access token
+  res.status(200).send(`${res_token}`);
+});
+
+// OAuth2 Access Token getter
+app.post('/set_access_token', (req, res) => {
+  console.log(req.body);
+  const access_token = req.body.access_token;
+  const result = 'fail';
+
+  // Check with process.env.CLIENT_ID and process.env.CLIENT_SECRET
+  console.log(`Access Token received successfully, payload: ${access_token}`);
+
+  // Send an access token
+  res.status(200).send(`${result}`);
+});
+
+// OAuth2 refresh token
+app.post('/refresh_access_token', (req, res) => {
+  console.log(req.body);
+  const refresh_token = req.body.refresh_token;
+  const result = 'fail';
+
+  // Check with process.env.CLIENT_ID and process.env.CLIENT_SECRET
+  console.log(`Refreshed access token successfully, payload: ${refresh_token}`);
+
+  // Send an access token
+  res.status(200).send(`${result}`);
+});
+
 // Start the server
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 3000', process.env.ZAPIER_REDIRECT_URI);
 });
