8e4c85c55a51eebfd49aab5ec8989fe664e24768
0xsnowman <eth.snowman@gmail.com>
2023-10-07 11:07:41 +0800

    k

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/authorize', (req, res) => {
   // Check with process.env.CLIENT_ID and process.env.CLIENT_SECRET
   console.log(`Authorized successfully, payload: {${clientId}, ${state}, ${redirectUri}, ${responseType}}`);
 
-  const authorizeUrl = `${process.env.ZAPIER_REDIRECT_URI}?client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}`;
+  const authorizeUrl = `${process.env.ZAPIER_REDIRECT_URI}?client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&code=code`;
 
   // Send an access token
   res.redirect(authorizeUrl);
