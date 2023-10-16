e1a0ca8e750a04338ae00c46e071caf2f7985387
0xsnowman <eth.snowman@gmail.com>
2023-10-16 21:41:33 +0800

    scope fix

--- src/index.js
+++ src/index.js
@@ -36,8 +36,10 @@ app.get(
   "/google",
   passport.authenticate("google", {
     scope: [
-      "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/userinfo.profile",
+      // "https://www.googleapis.com/auth/userinfo.email",
+      // "https://www.googleapis.com/auth/userinfo.profile",
+      "email",
+      "profile",
       "openid",
     ],
   })
