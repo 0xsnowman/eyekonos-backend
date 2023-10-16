46356f0bbb4f1398633cdb8cbc61da544e9c8bb3
0xsnowman <eth.snowman@gmail.com>
2023-10-16 20:42:23 +0800

    backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 const bodyParser = require('body-parser');
 const { getXataClient } = require("./xata");
 
@@ -7,52 +9,63 @@ const xata = getXataClient();
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    name: "google-auth-session",
+    keys: ["key1", "key2"],
+  })
+);
 
-app.post('/tickets', async (req, res) => {
-  const email = req.body.email;
-  const ticket_id = req.body.ticket_id;
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.get("/", async (req, res) => {
   await xata.db.execution_result.create({
-    execution_result: `Ticket data received. Payload: {${email}, ${ticket_id}}`,
+    execution_result: `Root path called: Not logged in state`,
   });
-  res.status(200).send(`Ticket data received. Payload: {${email}, ${ticket_id}}`);
+  res.json({ message: "You are not logged in" });
 });
 
-app.get('/authorize', async (req, res) => {
+app.get("/failed", async (req, res) => {
   await xata.db.execution_result.create({
-    execution_result: `authorized url: https://auth-json-server.zapier-staging.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.ZAPIER_REDIRECT_URI}&response_type=code`,
+    execution_result: `Failed.`,
   });
-  res.redirect(`https://auth-json-server.zapier-staging.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.ZAPIER_REDIRECT_URI}&response_type=code`)
+  res.send("Failed");
+});
+app.get("/success", async (req, res) => {
+  await xata.db.execution_result.create({
+    execution_result: `Success.`,
+  });
+  res.send(`Welcome ${req.user.email}`);
 });
 
-app.post('/token', async (req, res) => {
-  const response = await req.request({
-    url: 'https://auth-json-server.zapier-staging.com/oauth/access-token',
-    method: 'POST',
-    body: {
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      grant_type: 'authorization_code',
-      code: 4444,
-    },
-    headers: { 'content-type': 'application/x-www-form-urlencoded' },
-  });
-  await xata.db.execution_result.create({
-    execution_result: `access token: ${response.access_token}`,
-  });
-  return {
-    access_token: response.access_token
+app.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "openid",
+    ],
+  }),
+  async (req, res) => {
+    await xata.db.execution_result.create({
+      execution_result: `/google path called. authenticating.`,
+    });
   }
-});
+);
 
-app.get('/me', async (req, res) => {
-  await xata.db.execution_result.create({
-    execution_result: `Hello<br><a href="/auth">Log in with Zapier</a>`,
-  });
-  res.send('Hello<br><a href="/auth">Log in with Zapier</a>');
-});
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failed",
+  }),
+  async (req, res) => {
+    await xata.db.execution_result.create({
+      execution_result: `/google/callback path called. redirecting.`,
+    });
+    res.redirect("/success");
+  }
+);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
-});
+app.listen(port, () => console.log("server running on port " + port));
